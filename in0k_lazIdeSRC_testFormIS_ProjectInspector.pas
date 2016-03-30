d39ff4b18776f17d6d9314c4dbcee835230822a8
in0k <slvr9@mail.ru>
2016-03-30 15:35:55 +0500

    add

--- in0k_lazIdeSRC_testFormIS_ProjectInspector.pas
+++ in0k_lazIdeSRC_testFormIS_ProjectInspector.pas
@@ -1,4 +1,4 @@
-unit in0k_lazIdeSRC_testFormIS_PackageEditor;
+unit in0k_lazIdeSRC_testFormIS_ProjectInspector;
 
 {$mode objfpc}{$H+}
 interface
@@ -6,7 +6,7 @@ interface
 uses LCLVersion, //< версия Lazarusa
      Forms;
 
-// проверить что форма является Экземляром: PackageEditor
+// проверить что форма является Экземляром: ProjectInspector
 function toConfirm(const testForm:TCustomForm):boolean; {$ifOPT D-}inline;{$endIf}
 
 implementation
@@ -14,13 +14,13 @@ implementation
 function toConfirm(const testForm:TCustomForm):boolean;
 begin {Форма НЕ определена в IDEIntf, поэтому влоб, через название}
     {$if     (lcl_fullversion>=1060004 )}
-        result:=testForm.ClassNameIs('TPackageEditorForm');
+        result:=testForm.ClassNameIs('TProjectInspectorForm');
     {$elseif (lcl_fullversion>=1060002 )}
-        result:=testForm.ClassNameIs('TPackageEditorForm');
+        result:=testForm.ClassNameIs('TProjectInspectorForm');
     {$elseif (lcl_fullversion>=1040400 )}
-        result:=testForm.ClassNameIs('TPackageEditorForm');
+        result:=testForm.ClassNameIs('TProjectInspectorForm');
     {$else} {$WARNING 'NOT Tested in this LazarusIDE version'}
-        result:=testForm.ClassNameIs('TPackageEditorForm');
+        result:=testForm.ClassNameIs('TProjectInspectorForm');
     {$endif}
 end;
 
